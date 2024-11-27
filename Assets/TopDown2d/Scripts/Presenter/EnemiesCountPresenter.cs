8bc3c653d0d59cc69551a46e5e9b58f7ef3571eb
kakimotos <ikaruga.hajme@gmail.com>
2024-11-27 10:52:03 +0900

    UI実装中

---
+++ Assets/TopDown2d/Scripts/Presenter/EnemiesCountPresenter.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using TopDown2D.Scripts.Model;
+using UnityEngine;
+
+public class EnemiesCountPresenter : MonoBehaviour
+{
+    [SerializeField] private MapManager mapManager;
+    private TextMeshProUGUI _enemiesCountText;
+    
+    
+}
